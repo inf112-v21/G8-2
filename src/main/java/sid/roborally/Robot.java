d360e6077be757410204c3a5dfb2b9cb63f30075
MARKUS EDLIN <MARKUS.EDLIN@student.uib.no>
2021-02-17 18:03:16 +0100

    Added health and getHealth() and list of flags to Robot Class with test.

--- src/main/java/sid/roborally/Robot.java
+++ src/main/java/sid/roborally/Robot.java
@@ -1,15 +1,21 @@
 package sid.roborally;
 
+import java.util.ArrayList;
+
 public class Robot extends GridObject {
 
     private boolean isDead;
     private boolean hasWon;
+    ArrayList<Flag> flags;
+    private int health;
     Direction orientation;
 
     public Robot(int x, int y) {
         super(x,y);
         this.isDead = false;
         this.hasWon = false;
+        this.health = 1;
+        flags = new ArrayList<>();
     }
 
     public boolean isDead() {
@@ -18,5 +24,9 @@ public class Robot extends GridObject {
 
     public boolean hasWon() {
         return this.hasWon;
+    }
+
+    public int getHealth() {
+        return this.health;
     }
 }
