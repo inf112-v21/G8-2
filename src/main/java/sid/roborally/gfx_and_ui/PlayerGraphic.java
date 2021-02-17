f95e47a80bde9fa20b09c168be4984b55a6eae9f
Daniel Janols <daniel.b.janols@hotmail.com>
2021-02-17 18:01:11 +0100

    PlayerGraphic lagd og til en viss grad implementert

---
+++ src/main/java/sid/roborally/gfx_and_ui/PlayerGraphic.java
@@ -0,0 +1,52 @@
+package sid.roborally.gfx_and_ui;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
+import sid.roborally.MockPlayer;
+
+/**
+ * <p>This class will be connected to the Player class and
+ * its responsibility is to control the grahics part of a Player/User-entity</p>
+ *
+ * @author Daniel-J
+ */
+public class PlayerGraphic {
+
+    //Player's state-cells (graphics for different states)
+    TiledMapTileLayer.Cell playerLive;
+    TiledMapTileLayer.Cell playerDead;
+    TiledMapTileLayer.Cell playerWin;
+
+    MockPlayer player;
+
+    //TODO: Change MockPlayer
+    public PlayerGraphic(MockPlayer player, int playerTextureIndex)
+    {
+        this.player = player;
+
+        /* Giving player its different graphics */
+        setSelectedStateGraphics(playerTextureIndex);
+    }
+
+    private void setSelectedStateGraphics(int index)
+    {
+        TextureRegion tex = new TextureRegion(new Texture("player.png"));
+        //[][0]:Alive; [][1]:Dead; [][2]:Won;
+        TextureRegion[][] playerTextures = tex.split(300,300);
+
+        playerLive = new TiledMapTileLayer.Cell().setTile(new StaticTiledMapTile(playerTextures[index][0]));
+        playerDead = new TiledMapTileLayer.Cell().setTile(new StaticTiledMapTile(playerTextures[index][1]));
+        playerWin = new TiledMapTileLayer.Cell().setTile(new StaticTiledMapTile(playerTextures[index][2]));
+    }
+
+    public TiledMapTileLayer.Cell getPlayerTexture()
+    {
+        if(player.hasWon()) return playerWin;
+        if(player.isDead()) return playerDead;
+        else return playerLive;
+    }
+
+    public MockPlayer getPlayer() { return player; }
+}
