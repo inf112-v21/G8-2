b48cc16c20a6d52dd9786dda264b93164475b7f5
MARKUS EDLIN <MARKUS.EDLIN@student.uib.no>
2021-02-18 17:40:01 +0100

    Added Grid class with a grid double arraylist containing a HashSet<GridObject>.. added methods containsRobot, checkInBounds, getGridObjectsFromPosition, addGridObjectToGrid and moveGridObjectToNewPosition

---
+++ src/main/java/sid/roborally/game_mechanics/Grid.java
@@ -0,0 +1,70 @@
+package sid.roborally.game_mechanics;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Grid {
+    ArrayList<ArrayList<HashSet<GridObject>>> grid;
+
+    private int width;
+    private int height;
+
+    public Grid(int w, int h) {
+        this.width = w;
+        this.height = h;
+        grid = new ArrayList<>();
+        for(int i = 0; i<width; i++){
+            grid.add(new ArrayList<>());
+            for(int j = 0; j<height; j++){
+                grid.get(i).add(new HashSet<>());
+            }
+        }
+    }
+
+    public boolean containsRobot(Position pos) {
+        int x = pos.getX();
+        int y = pos.getY();
+        HashSet<GridObject> thisPosition = grid.get(x).get(y);
+        for(GridObject o : thisPosition) if(o instanceof Robot) return true;
+        return false;
+    }
+
+    public boolean checkInBounds(Position pos) {
+        return ((pos.getX()<width && pos.getX()>=0) && (pos.getY()<height && pos.getY()>=0));
+    }
+
+    public HashSet<GridObject> getGridObjectsFromPosition(Position pos) {
+        return grid.get(pos.getX()).get(pos.getY());
+    }
+
+    /**
+     * Adds to grid if position is inbounds and if there isn't a robot at that same position
+     *
+     * @param o GridObject
+     */
+    public void addGridObjectToGrid(GridObject o) {
+        if(checkInBounds(o.getPosition())) grid.get(o.getPosition().getX()).get(o.getPosition().getY()).add(o);
+    }
+
+
+    /**
+     * Removes current GridObject from grid.
+     * Changes position of GridObject to given newPos.
+     * Adds GridObject to grid with new position.
+     *
+     * @param o GridObject to move
+     * @param newPos new Position to move GridObject to
+     */
+    public void moveGridObjectToNewPosition(GridObject o, Position newPos) {
+        if(getGridObjectsFromPosition(o.getPosition()).contains(o)){
+            removeGridObjectFromGrid(o);
+            o.setPosition(newPos.getX(), newPos.getY());
+            addGridObjectToGrid(o);
+        }
+    }
+
+    public void removeGridObjectFromGrid(GridObject o) {
+        if(getGridObjectsFromPosition(o.getPosition()).contains(o)) grid.get(o.getPosition().getX()).get(o.getPosition().getY()).remove(o);
+        else throw new IllegalArgumentException("Position"+ o.getPosition() + "does not contain this object.");
+    }
+}
--- src/test/java/sid.roborally/game_mechanics/GridTest.java
+++ src/test/java/sid.roborally/game_mechanics/GridTest.java
@@ -1,5 +1,96 @@
 package sid.roborally.game_mechanics;
 
-public class GridTest {
+import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import static org.junit.Assert.*;
+
+public class GridTest {
+    Grid g;
+
+    @Test
+    public void CreateEmptyGridTest() {
+        Grid g = new Grid(5,5);
+
+        ArrayList<ArrayList<HashSet<GridObject>>> expected = new ArrayList<>();
+        for(int i = 0; i<5; i++){
+            expected.add(new ArrayList<>());
+            for(int j = 0; j<5; j++){
+                expected.get(i).add(new HashSet<>());
+            }
+        }
+        ArrayList<ArrayList<HashSet<GridObject>>> result = g.grid;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void CheckIfRobotIsAtPositionTest() {
+        int x = 0;
+        int y = 0;
+        Robot r = new Robot(x,y);
+        g = new Grid(5,5);
+        g.grid.get(x).get(y).add(r);
+        assertTrue(g.containsRobot(r.getPosition()));
+    }
+
+    @Test
+    public void CheckIfPositionIsWithinBoardTest() {
+        g = new Grid(5,5);
+        for(int x = 0; x<5; x++){
+            for(int y = 0; y<5; y++){
+                Position pos = new Position(x,y);
+                boolean result = g.checkInBounds(pos);
+                assertTrue(result);
+            }
+        }
+    }
+
+    @Test
+    public void AddGridObjectToGridTest() {
+        g = new Grid(5,5);
+        Robot r = new Robot(1,1);
+        g.addGridObjectToGrid(r);
+        HashSet<GridObject> result = g.getGridObjectsFromPosition(new Position(1,1));
+        HashSet<GridObject> expected = new HashSet<>();
+        expected.add(r);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void MoveGridObjectToNewPositionTest() {
+        g = new Grid(5,5);
+        Robot r = new Robot(1,1);
+        g.addGridObjectToGrid(r);
+        g.moveGridObjectToNewPosition(r, new Position(2,2));
+        HashSet<GridObject> result = g.getGridObjectsFromPosition(new Position(2,2));
+        HashSet<GridObject> expected = new HashSet<>();
+        expected.add(r);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void removeGridObjectFromGridTest() {
+        g = new Grid(5,5);
+        Robot r = new Robot(1,1);
+        HashSet<GridObject> expected = new HashSet<>();
+        g.addGridObjectToGrid(r);
+        HashSet<GridObject> result = g.getGridObjectsFromPosition(r.getPosition());
+        g.removeGridObjectFromGrid(r);
+
+        assertEquals(expected, result);
+    }
+    /*
+    @Test
+    public void CheckGridPositionForGridObjectTest() {
+        g = new Grid(5,5);
+        Robot r = new Robot(1,1);
+        HashSet<GridObject> result = g.getFromPosition(r.getPosition());
+        HashSet<GridObject> expected = new HashSet<>();
+        expected.add(r);
+        assertEquals(expected,result);
+    }
+
+     */
 }
