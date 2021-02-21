c5835e496204663a45bf619c49fa1cb38d0ec026
Terje Trommestad <Terjetrommestad@gmail.com>
2021-02-21 16:31:45 +0100

    Fikset Direction metoder og tester

--- src/main/java/sid/roborally/game_mechanics/Direction.java
+++ src/main/java/sid/roborally/game_mechanics/Direction.java
@@ -1,76 +1,38 @@
 package sid.roborally.game_mechanics;
 
 /**
+ *
  *  Directions that can be used by any GridObject.
- *(
+ *  Rotate methods for right, left and 180.
  *
  *  @author Terje Trommestad
  */
 
-
-//TODO: 18/02/2021 Decide to use Switch or Rotate. Intended usage with switch: Direction.NORTH.left() returns WEST
-//
-//SWITCH
-
-
-
 public enum Direction {
-    NORTH {
-        Direction left() {
-            return WEST;
-        }
-
-        Direction right() {
-            return EAST;
-        }
-    },
-    EAST {
-        Direction left() {
-            return NORTH;
-        }
-
-        Direction right() {
-            return SOUTH;
-        }
-    },
-    SOUTH {
-        Direction left() {
-            return EAST;
-        }
-
-        Direction right() {
-            return WEST;
-        }
-    },
-    WEST {
-        Direction left() {
-            return SOUTH;
-        }
-
-        Direction right() {
-            return NORTH;
-        }
-
-        //abstract Direction left();
-        //abstract Direction right();
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST;
 
 
-        // TODO: 18/02/2021 better solution?
-        // Rotate right (90 degrees clocwise)
-        public Direction rotateRight() {
-            return values()[(ordinal() + 1) % 4];
-        }
+    Direction rotateRight() {
+        return values()[(ordinal() + 1) % 4];
+    }
 
-        // Rotate 180 degrees
-        // public Direction rotate180() {
-        //     return values()[(ordinal() + 2) % 4];
-        // }
+    Direction rotate180() {
+         return values()[(ordinal() + 2) % 4];
+     }
 
-        // Rotate left (270 degrees clockwise (90 counterclockwise))
-        public Direction rotateLeft() {
-            return values()[(ordinal() + 3) % 4];
-        }
-
-
+    Direction rotateLeft() {
+        return values()[(ordinal() + 3) % 4];
     }
 }
+
+
+
+
+
+
+
+
+
--- src/test/java/sid.roborally/game_mechanics/DirectionTest.java
+++ src/test/java/sid.roborally/game_mechanics/DirectionTest.java
@@ -6,28 +6,42 @@ import static org.junit.Assert.*;
 
 public class DirectionTest {
 
-    GridObject testbot1 = new GridObject(1,2);
+    GridObject testobject = new GridObject(1,2);
+    Robot testbot = new Robot(3,3);
     Direction dir1;
 
 
     @Test
-    public void left() {
+    public void TurnLeftTest() {
+        testobject.setOrientation(Direction.SOUTH);
+        testobject.setOrientation(testobject.getOrientation().rotateLeft());
+        assertEquals(testobject.getOrientation(),Direction.EAST);
+
+    }
+
+    @Test
+    public void TurnRightTest() {
+        //System.out.println("RIGHT RIGHT RIGHT");
+        testobject.setOrientation(Direction.NORTH);
+        //System.out.println(testobject.getOrientation());
+        testobject.setOrientation(testobject.getOrientation().rotateRight());
+        //System.out.println(testobject.getOrientation());
+        assertEquals(testobject.getOrientation(),Direction.EAST);
+
+
+    }
+
+    @Test
+    public void Turn180Test() {
         dir1 = Direction.NORTH;
-        assertTrue(dir1 == Direction.NORTH);
-        dir1 = Direction.WEST;
-        assertTrue(dir1 == Direction.WEST);
-
-    }
-
-    @Test
-    public void right() {
-    }
-
-    @Test
-    public void values() {
-        System.out.println(testbot1.getOrientation());
-        testbot1.setOrientation(Direction.SOUTH);
-        System.out.println(testbot1.getOrientation());
+        dir1 = dir1.rotateRight();
+        dir1 = dir1.rotateRight();
+        //System.out.println(dir1);
+        dir1 = dir1.rotate180();
+        assertEquals(dir1,Direction.NORTH);
+        testbot.setOrientation(dir1);
+        testbot.setOrientation(testbot.getOrientation().rotate180());
+        assertEquals(testbot.getOrientation(),Direction.SOUTH);
     }
 
     @Test
