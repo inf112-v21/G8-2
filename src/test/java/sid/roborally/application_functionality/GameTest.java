7afd58a5f447804dd88740ae29b878106aebee39
djanols <41550448+djanols@users.noreply.github.com>
2021-02-22 18:08:01 +0100

    Utvidet testdekning for Game i GameTest

--- src/test/java/sid/roborally/application_functionality/GameTest.java
+++ src/test/java/sid/roborally/application_functionality/GameTest.java
@@ -2,12 +2,17 @@ package sid.roborally.application_functionality;
 
 import org.junit.Before;
 import org.junit.Test;
+import sid.roborally.game_mechanics.Direction;
 import sid.roborally.game_mechanics.Game;
 import sid.roborally.game_mechanics.Position;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
+/**
+ * <h3>GameTest</h3>
+ * <p>This class tests the Game-class to check that all
+ *    it features work as intended.</p>
+ */
 public class GameTest {
 
     Game game;
@@ -17,6 +22,7 @@ public class GameTest {
     public void addElements()
     {
         game = new Game();
+        game.newGrid(5,5);
 
         p1 = new Player(new Position(1,1), false);
         p2 = new Player(new Position(2,2), false);
@@ -24,6 +30,9 @@ public class GameTest {
     }
 
 
+    /**
+     * <p>Checks that a game-instance will keep track of its players.</p>
+     */
     @Test
     public void gameKeepsTrackOfPlayers()
     {
@@ -37,4 +46,49 @@ public class GameTest {
 
         assertFalse(game.hasPlayer(p2));
     }
+
+    /**
+     * <p>Checks to see if game can move the player's robot in grid.</p>
+     */
+    @Test
+    public void canMovePlayerRobot()
+    {
+        game.addPlayer(p1); //Player's robot should have position (1,1)
+        assertEquals(new Position(1,1).toString(),
+                p1.getRobot().getPosition().toString());
+
+        //Moving player's robot.
+        game.movePlayerRobot(p1, Direction.NORTH);
+        game.movePlayerRobot(p1, Direction.EAST);
+        game.movePlayerRobot(p1, Direction.EAST);
+
+        //New position should be (1+1+1, 1+1) = (3,2)
+        assertEquals(new Position(3,2).toString(),
+                p1.getRobot().getPosition().toString());
+    }
+
+    /**
+     * <p>Checks to see that the game can retrieve a local player.</p>
+     */
+    @Test
+    public void canGetLocalPlayer()
+    {
+        game.addPlayer(p1);
+        game.addPlayer(p2);
+        game.addPlayer(p3);
+
+        p2.setLocal();
+
+        assertFalse(game.getLocal().equals(p1));
+        assertTrue(game.getLocal().equals(p2));
+        assertFalse(game.getLocal().equals(p3));
+
+        p2.setAI();
+        p3.setLocal();
+
+        assertFalse(game.getLocal().equals(p1));
+        assertFalse(game.getLocal().equals(p2));
+        assertTrue(game.getLocal().equals(p3));
+    }
+
 }
