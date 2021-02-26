1232cfcd0dfce15c236a6f923cef39891550df2c
MARKUS EDLIN <MARKUS.EDLIN@student.uib.no>
2021-02-26 11:46:42 +0100

    Refactored TildIDReference

--- src/main/java/sid/roborally/application_functionality/TileIDReference.java
+++ src/main/java/sid/roborally/application_functionality/TileIDReference.java
@@ -1,16 +1,14 @@
 package sid.roborally.application_functionality;
 
-import sid.roborally.game_mechanics.grid.Flag;
-
-import java.util.HashMap;
-
 public class TileIDReference {
 
     public static int flagIndexToId(int flag_index){
-        if(flag_index == 55) return 1;
-        else if(flag_index == 63) return 2;
-        else if(flag_index == 71) return 3;
-        else return 4;
+        switch(flag_index){
+            case 55: return 1;
+            case 63: return 2;
+            case 71: return 3;
+            default: return 4;
+        }
     }
 
 }
