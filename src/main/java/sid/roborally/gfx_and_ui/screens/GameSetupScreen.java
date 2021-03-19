37adcce96dbb5f9a22083166e6edf040573c8d34
andreashenriksen <andreassoland.99@gmail.com>
2021-03-19 12:23:45 +0100

    Moved some code to their own methods, and added a drop-down menu to select amount of players.

--- src/main/java/sid/roborally/gfx_and_ui/screens/GameSetupScreen.java
+++ src/main/java/sid/roborally/gfx_and_ui/screens/GameSetupScreen.java
@@ -15,7 +15,14 @@ import sid.roborally.application_functionality.RRApplication;
 import sid.roborally.application_functionality.reference.Map;
 import sid.roborally.gfx_and_ui.AppListener;
 
+import java.util.ArrayList;
 
+/**
+ * <h3>GameSetupScreen</h3>
+ * <p>A screen where you are able to setup a game.</p>
+ *
+ * @author Andreas Henriksen & Terje Trommestad
+ */
 public class GameSetupScreen implements Screen {
 
     private final AppListener appListener;
@@ -24,12 +31,15 @@ public class GameSetupScreen implements Screen {
     private Skin skin;
     private Button startGameButton, backButton;
     private SelectBox<String> mapBox;
+    private SelectBox<Integer> playerBox;
     private Table menuTable;
     private RRApplication rr_app;
+    private ArrayList<Player> players;
 
     public GameSetupScreen(AppListener appListener) {
         this.appListener = appListener;
         rr_app = appListener.getRRApp();
+        this.players = new ArrayList<>();
 
         cam = new OrthographicCamera();
         cam.setToOrtho(false, 800, 480);
@@ -40,11 +50,16 @@ public class GameSetupScreen implements Screen {
         skin = appListener.getSkin();
 
         Array<String> mapNames = new Array<>();
-        for (Map map : Map.values())
-            mapNames.add(map.name());
+        addMaps(mapNames);
 
         mapBox = new SelectBox<>(skin, "default");
         mapBox.setItems(mapNames);
+
+        /* Creating a drop down menu to select amount of players */
+        playerBox = new SelectBox<>(skin, "default");
+        playerBox.setPosition(Gdx.graphics.getWidth() / 2f + 150f, Gdx.graphics.getHeight() / 2f + 83f);
+        playerBox.setSize(125, 35);
+        playerBox.setItems(1, 2, 3, 4, 5, 6, 7, 8);
 
         startGameButton = new TextButton("Singleplayer",skin,"default");
         startGameButton.setTransform(true);
@@ -52,24 +67,15 @@ public class GameSetupScreen implements Screen {
         backButton = new TextButton("Back",skin,"default");
         backButton.setTransform(true);
 
-        Player localPlayer = new Player(1, true);
-        localPlayer.setLocal();
-
         menuTable = new Table();
-        menuTable.add(mapBox);
-        menuTable.row();
-        menuTable.add(startGameButton);
-        menuTable.row();
-        menuTable.add(backButton);
-        menuTable.setFillParent(true);
+        addButtons(menuTable);
 
 
         startGameButton.addListener(new InputListener(){
             @Override
             public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-                rr_app.getGameRunner().addPlayer(localPlayer);
-                rr_app.getGameRunner().setUpGame(Map.values()[mapBox.getSelectedIndex()]);
-                rr_app.startGame();
+                addPlayers(players, playerBox.getSelected());
+                setUpGame(players);
                 appListener.setScreen(new GameScreen(appListener));
             }
             @Override
@@ -88,7 +94,58 @@ public class GameSetupScreen implements Screen {
             }
         });
 
+        stage.addActor(playerBox);
         stage.addActor(menuTable);
+    }
+
+    /**
+     * Starts the game with the selected map, and the selected amount of players
+     * @param players a list of players
+     */
+    private void setUpGame(ArrayList<Player> players) {
+        for (Player player : players)
+            rr_app.getGameRunner().addPlayer(player);
+        rr_app.getGameRunner().setUpGame(Map.values()[mapBox.getSelectedIndex()]);
+        rr_app.startGame();
+    }
+
+    /**
+     * Adds all available maps to an Array
+     * @param maps array of maps
+     */
+    private void addMaps(Array<String> maps) {
+        for (Map map : Map.values())
+            maps.add(map.name());
+    }
+
+    /**
+     * Adds all needed buttons to the screen
+     * @param menuTable table to hold all buttons and drop down menus
+     */
+    private void addButtons(Table menuTable) {
+        menuTable.add(mapBox);
+        menuTable.row();
+        menuTable.add(startGameButton);
+        menuTable.row();
+        menuTable.add(backButton);
+        menuTable.setFillParent(true);
+    }
+
+    /**
+     * Adds the amount of players you want to have into an ArrayList
+     * @param players ArrayList to hold players
+     * @param amount amount of players
+     */
+    private void addPlayers(ArrayList<Player> players, int amount) {
+        Player localPlayer = new Player(1, true);
+        localPlayer.setLocal();
+        players.add(localPlayer);
+        if (amount > 1) {
+            for (int i = 2; i <= amount; i++) {
+                Player newPlayer = new Player(i, true);
+                players.add(newPlayer);
+            }
+        }
     }
 
     @Override
