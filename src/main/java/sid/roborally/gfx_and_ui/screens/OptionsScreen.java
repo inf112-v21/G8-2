8aa86a6dad479202fb80ac87e1092e2e9f3da0b6
andreashenriksen <andreassoland.99@gmail.com>
2021-03-23 15:50:30 +0100

    Added a fullscreen option to OptionsScreen

--- src/main/java/sid/roborally/gfx_and_ui/screens/OptionsScreen.java
+++ src/main/java/sid/roborally/gfx_and_ui/screens/OptionsScreen.java
@@ -5,14 +5,14 @@ import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Scaling;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import sid.roborally.gfx_and_ui.AppListener;
 
@@ -28,7 +28,7 @@ public class OptionsScreen implements Screen {
     private OrthographicCamera cam;
     private Stage stage;
     private Skin skin;
-    private Button backButton;
+    private Button fullscreenButton, windowedButton, backButton;
     private Table table;
     private int buttWidth,buttHeight;
 
@@ -45,6 +45,8 @@ public class OptionsScreen implements Screen {
         table.setFillParent(true);
         table.setBackground(new TextureRegionDrawable(new TextureRegion(
                 new Texture("assets/application_skin/youhavenopower.jpg"))));
+        table.center();
+
 
         cam = new OrthographicCamera();
         cam.setToOrtho(false, 800, 480);
@@ -52,17 +54,48 @@ public class OptionsScreen implements Screen {
         buttHeight = appListener.getButtHeight();
 
                 stage = new Stage(new ScreenViewport());
-        stage.addActor(table);
 
         Gdx.input.setInputProcessor(stage);
 
         skin = appListener.getSkin();
 
-        backButton = new TextButton("Back", skin, "default");
-        backButton.setSize(buttWidth, buttHeight);
-        backButton.setPosition(Gdx.graphics.getWidth() / 2f - 80f, Gdx.graphics.getHeight() / 2f);
-        backButton.setTransform(true);
+        fullscreenButton = new TextButton("Fullscreen", skin, "default");
 
+        windowedButton = new TextButton("Windowed", skin, "default");
+
+        backButton = new TextButton("Back", skin, "default");
+
+        table.add(fullscreenButton);
+        table.row();
+        table.add(windowedButton);
+        table.row();
+        table.add(backButton);
+
+
+        fullscreenButton.addListener(new InputListener() {
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
+                new FitViewport(9, 16);
+            }
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
+        });
+        windowedButton.addListener(new InputListener() {
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                if (Gdx.graphics.isFullscreen()) {
+                    Gdx.graphics.setWindowedMode(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2+200);
+                    new FitViewport(9, 16);
+                }
+            }
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
+        });
         backButton.addListener(new InputListener() {
             @Override
             public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
@@ -75,7 +108,7 @@ public class OptionsScreen implements Screen {
                 return true;
             }
         });
-        stage.addActor(backButton);
+        stage.addActor(table);
     }
 
     @Override
@@ -99,6 +132,7 @@ public class OptionsScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
