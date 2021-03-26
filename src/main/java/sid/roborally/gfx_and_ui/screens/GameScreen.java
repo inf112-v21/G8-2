56797125213f5f3be56b76f1cbe91ac7571758ee
Daniel Janols <daniel.b.janols@hotmail.com>
2021-03-26 10:04:49 +0100

    Refactor GameScreen cleaned up somewhat

--- src/main/java/sid/roborally/gfx_and_ui/screens/GameScreen.java
+++ src/main/java/sid/roborally/gfx_and_ui/screens/GameScreen.java
@@ -10,7 +10,6 @@ import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Array;
@@ -32,40 +31,53 @@ import java.util.ArrayList;
  */
 public class GameScreen extends InputAdapter implements ApplicationListener, Screen {
 
-    final AppListener appListener;
+    private final AppListener appListener;
     private RRApplication rr_app;
     private GameRunner grunner;
 
-    private Stage uiStage;
-    private Viewport uiView;
-
-    private float startZoomOut = 1;
-
-    private static final String controlTableBackground = "assets/application_skin/tempCBackground.png";
-    private Table controlTable; //Where you program the robot, and manage your decisions.
-
-    /* Info-table*/
-    private Table infoTable;
-    private Window cardWindow;
-
-    /* Control-table*/
-    private SelectBox<String> reg1, reg2, reg3, reg4, reg5;
-    private String baseOptionReg1 = "Register One",
-            baseOptionReg2 = "Register Two",
-            baseOptionReg3 = "Register Three",
-            baseOptionReg4 = "Register Four",
-            baseOptionReg5 = "Register Five";
-    private int reg1index = -1, reg2index = -1, reg3index = -1, reg4index = -1,reg5index = -1; //Chosen card index (-1) non chosen.
-
-    private Button commitButton;
+    /*
+     * * * Game-information
+     */
 
     private ArrayList<Card> givenCards;
     private ArrayList<Integer> chosenCardsIndices;
     private ArrayList<Card> chosenCards;
-
-    private InputMultiplexer inputMultiplexer;
-
     private Player localPlayer;
+
+    /*
+     * * * Scene2d elements
+     */
+
+    private Stage uiStage;
+    private Viewport uiView;
+
+    /* Info-table */
+    private Table infoTable; //Where we display information.
+    private Window cardWindow;
+
+    /* Control-table*/
+    private static final String CONTROL_TABLE_BACKGROUND = "assets/application_skin/tempCBackground.png";
+    private Table controlTable; //Where you program the robot, and manage your decisions.
+
+    private SelectBox<String> reg1, reg2, reg3, reg4, reg5;
+    private String
+            baseOptionReg1 = "Register One",
+            baseOptionReg2 = "Register Two",
+            baseOptionReg3 = "Register Three",
+            baseOptionReg4 = "Register Four",
+            baseOptionReg5 = "Register Five";
+    private int //Chosen card index (-1) non chosen.
+            reg1index = -1,
+            reg2index = -1,
+            reg3index = -1,
+            reg4index = -1,
+            reg5index = -1;
+
+    private Button commitButton; //Commits cards
+
+    /*
+     * * * Map-rendering
+     */
 
     /* Renderer and camera */
     private OrthogonalTiledMapRenderer rend;
@@ -75,6 +87,17 @@ public class GameScreen extends InputAdapter implements ApplicationListener, Scr
     private int mapWidth, mapHeight;
     private static final float MAP_UNIT= 300f; //Time position times mapUnit to find cam-position.
     private static final float CAM_OFFSET_X = 150 + 600, CAM_OFFSET_Y = 150 - 250;
+    private static final float START_ZOOM_OUT = 1; //Default zoom is too close, so need to zoom out.
+
+    /*
+     * Collective input-handling
+     */
+
+    private InputMultiplexer inputMultiplexer;
+
+    /*
+     * * * * VARIABLES END
+     */
 
     public GameScreen(final AppListener appListener) {
         this.appListener = appListener;
@@ -94,35 +117,80 @@ public class GameScreen extends InputAdapter implements ApplicationListener, Scr
         updateCardSelection();
     }
 
-    public void getInitialInfo() {
-        rr_app = appListener.getRRApp();
-        grunner = rr_app.getGameRunner();
-        grunner.giveGameScreen(this);
-        localPlayer = grunner.getLocal();
+    /*
+     * Backend-communication
+     */
 
-        /* Get map-value info */
-        mapWidth = grunner.getBoardWidth();
-        mapHeight = grunner.getBoardHeight();
+    /**
+     * <p>Gives GameRunner-instance currently chosen cards.</p>
+     */
+    private void giveGamerunnerChosenCards() {
+        grunner.giveGameCards(chosenCards);
+    }
 
-        /* Viewport used by every component */
-        uiView = new FitViewport(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+    /**
+     * <p>Gives GameScreen its new set of cards.</p>
+     * @param cards Given cards
+     */
+    public void giveCards(ArrayList<Card> cards) { givenCards = cards; }
 
-        /* Head of input-handling */
-        inputMultiplexer = new InputMultiplexer();
+    /**
+     * <p>Checks that every register has selected a card and that noe indices are duplicated.</p>
+     * @return Bool
+     */
+    private boolean cardSelectionValid() {
+        if(     reg1index == -1 ||
+                reg2index == -1 ||
+                reg3index == -1 ||
+                reg4index == -1 ||
+                reg5index == -1
+        ) return false;
+        for(int i = 0; i < 5; i++) {
+            int indexCard = chosenCardsIndices.get(i); //Should only occur once
+            int duplicateCounter = 0;
+            for(int index : chosenCardsIndices)
+                if(index == indexCard) duplicateCounter++;
+            if(duplicateCounter>1) return false;
+        }
+        return true;
+    }
+
+    /*
+     * * * * * HUD-updating
+     */
+
+    /**
+     * <p>Called when GUI has to be updated after changing values</p>
+     */
+    public void updateGUI() {
+        updateWindowCards();
+    }
+
+    /**
+     * <p>Updates cards displayed in info-window.</p>
+     */
+    public void updateWindowCards() {
+        cardWindow.clear();
+        for(Card card : givenCards) {
+            String steps = card instanceof StepCard //Possible step-information.
+                    ? ((StepCard) card).getSteps() + " "
+                    : "";
+            cardWindow.add(steps + card.getName() + "; Priority: " + card.getPriority());
+            cardWindow.row();
+        }
     }
 
     /**
      * <p>Updates selection-boxes and indexes.</p>
      */
     private void updateCardSelection() {
-        System.out.println("PING");
+
         Array<String> givenCardStrings = new Array<>();
         for(Card card : givenCards)
             if(card instanceof StepCard)
                 givenCardStrings.add(((StepCard) card).getSteps() + " " + card.getName() + ": Pri " + card.getPriority());
             else
                 givenCardStrings.add(card.getName() + ": Pri " + card.getPriority());
-
 
         Array<String> reg1array = new Array<>();
         reg1array.add(baseOptionReg1);
@@ -172,46 +240,28 @@ public class GameScreen extends InputAdapter implements ApplicationListener, Scr
         chosenCardsIndices.add(reg5index);
     }
 
-    private void setUpUIStage() {
-        uiStage = new Stage(uiView);
-
-        /* Sets up hud */
-        setUpInfoTable();
-        setUpControlTable();
-
-        inputMultiplexer.addProcessor(uiStage);
-
-        uiStage.act();
-        uiStage.draw();
-    }
+    /*
+     * * * * * Set-up
+     */
 
     /**
-     * <p>Checks that every register has selected a card and that noe indices are duplicated.</p>
-     * @return Bool
+     * <p>Gets all initial information. Called in constructor.</p>
      */
-    private boolean cardSelectionValid() {
-        if(     reg1index == -1 ||
-                reg2index == -1 ||
-                reg3index == -1 ||
-                reg4index == -1 ||
-                reg5index == -1
-        ) return false;
-        for(int i = 0; i < 5; i++) {
-            int indexCard = chosenCardsIndices.get(i); //Should only occur once
-            int duplicateCounter = 0;
-            for(int index : chosenCardsIndices)
-                if(index == indexCard) duplicateCounter++;
-            if(duplicateCounter>1) return false;
-        }
-        return true;
-    }
+    public void getInitialInfo() {
+        rr_app = appListener.getRRApp();
+        grunner = rr_app.getGameRunner();
+        grunner.giveGameScreen(this);
+        localPlayer = grunner.getLocal();
 
-    public void guiSleep(int millis) {
-        try {
-            Thread.sleep(millis);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        /* Get map-value info */
+        mapWidth = grunner.getBoardWidth();
+        mapHeight = grunner.getBoardHeight();
+
+        /* Viewport used by every component */
+        uiView = new FitViewport(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+
+        /* Head of input-handling */
+        inputMultiplexer = new InputMultiplexer();
     }
 
     /**
@@ -221,7 +271,7 @@ public class GameScreen extends InputAdapter implements ApplicationListener, Scr
         /* Bottom-table */
         controlTable = new Table();
         controlTable.setBackground(new TextureRegionDrawable(new TextureRegion(
-                new Texture(controlTableBackground))));
+                new Texture(CONTROL_TABLE_BACKGROUND))));
         controlTable.setSize(uiView.getWorldWidth(), uiView.getWorldHeight()/5);
         controlTable.left().top();
 
@@ -299,34 +349,30 @@ public class GameScreen extends InputAdapter implements ApplicationListener, Scr
         commitButton = new TextButton("Commit Program",appListener.getSkin());
         commitButton.setSize(appListener.getButtWidth(),appListener.getButtHeight());
         commitButton.addListener(
-        new InputListener(){
-            @Override
-            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-                updateCardSelection();
-                if(cardSelectionValid()) {
-                    chosenCards = new ArrayList<>();
-                    chosenCards.add(givenCards.get(reg1index));
-                    chosenCards.add(givenCards.get(reg2index));
-                    chosenCards.add(givenCards.get(reg3index));
-                    chosenCards.add(givenCards.get(reg4index));
-                    chosenCards.add(givenCards.get(reg5index));
-                    giveGamerunnerChosenCards();
-                    grunner.runRound();
-                }
-            }
-            @Override
-            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-                return true;
-            }
-        });
+                new InputListener(){
+                    @Override
+                    public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                        updateCardSelection();
+                        if(cardSelectionValid()) {
+                            chosenCards = new ArrayList<>();
+                            chosenCards.add(givenCards.get(reg1index));
+                            chosenCards.add(givenCards.get(reg2index));
+                            chosenCards.add(givenCards.get(reg3index));
+                            chosenCards.add(givenCards.get(reg4index));
+                            chosenCards.add(givenCards.get(reg5index));
+                            giveGamerunnerChosenCards();
+                            grunner.runRound();
+                        }
+                    }
+                    @Override
+                    public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                        return true;
+                    }
+                });
         controlTable.add(commitButton);
 
         /* Finally */
         uiStage.addActor(controlTable);
-    }
-
-    private void giveGamerunnerChosenCards() {
-        grunner.giveGameCards(chosenCards);
     }
 
     /**
@@ -335,7 +381,7 @@ public class GameScreen extends InputAdapter implements ApplicationListener, Scr
     private void setUpInfoTable() {
         infoTable = new Table();
         infoTable.setBackground(new TextureRegionDrawable(new TextureRegion(
-                new Texture(controlTableBackground))));
+                new Texture(CONTROL_TABLE_BACKGROUND))));
         infoTable.setSize(uiView.getWorldWidth()/4, uiView.getWorldHeight());
         infoTable.setPosition(uiView.getWorldWidth()*3/4,0);
 
@@ -346,30 +392,6 @@ public class GameScreen extends InputAdapter implements ApplicationListener, Scr
 
         /* Finally */
         uiStage.addActor(infoTable);
-    }
-
-    /**
-     * <p>Gives GameScreen its new set of cards.</p>
-     * @param cards Given cards
-     */
-    public void giveCards(ArrayList<Card> cards) { givenCards = cards; }
-
-    /**
-     * <p>Called when GUI has to be updated after changing values</p>
-     */
-    public void updateGUI() {
-        updateWindowCards();
-    }
-
-    public void updateWindowCards() {
-        cardWindow.clear();
-        for(Card card : givenCards) {
-            String steps = card instanceof StepCard //Possible step-information.
-                    ? ((StepCard) card).getSteps() + " "
-                    : "";
-            cardWindow.add(steps + card.getName() + "; Priority: " + card.getPriority());
-            cardWindow.row();
-        }
     }
 
     /**
@@ -390,46 +412,35 @@ public class GameScreen extends InputAdapter implements ApplicationListener, Scr
         /* Sets render's view to that of the camera */
         rend.setView(cam);
 
-        zoomCam(startZoomOut);
+        zoomCam(START_ZOOM_OUT);
 
         inputMultiplexer.addProcessor(this);
     }
 
-    private void updateRendWithCam() {
-        rend.setView(cam);
+    /**
+     * <p>Sets up the UI-stage (HUD)</p>
+     */
+    private void setUpUIStage() {
+        uiStage = new Stage(uiView);
+
+        /* Sets up hud */
+        setUpInfoTable();
+        setUpControlTable();
+
+        inputMultiplexer.addProcessor(uiStage);
+
+        uiStage.act();
+        uiStage.draw();
     }
 
-    private void centerCamOnMapCenter() {
-        float mapCenterX = MAP_UNIT * mapWidth/2;
-        float mapCenterY = MAP_UNIT * mapHeight/2;
-        cam.position.set(mapCenterX,mapCenterY,0);
-        cam.update();
-        rend.setView(cam);
-    }
-
-    private void centerCamOnPlayer(Player p) {
-        float playerX = p.getRobot().getPosition().getX() * MAP_UNIT;
-        float playerY = p.getRobot().getPosition().getY() * MAP_UNIT;
-
-        cam.position.set(playerX + CAM_OFFSET_X, playerY + CAM_OFFSET_Y,0);
-        cam.update();
-        updateRendWithCam();
-    }
-
-    @Override
-    public void show() {
-    }
+    /*
+     * * * * * Rendering
+     */
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0,1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
-
-        if (Gdx.input.isTouched()) {
-            Vector3 touchPos = new Vector3();
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-            System.out.println(touchPos.x + ":" + touchPos.y + ":" + touchPos.z);
-        }
 
         /* Sets cell-texture based on Player-texture-information */
         for(Player player : rr_app.getPlayers())
@@ -445,14 +456,54 @@ public class GameScreen extends InputAdapter implements ApplicationListener, Scr
         uiStage.draw();
     }
 
+    private void updateRendWithCam() {
+        rend.setView(cam);
+    }
+
+    /**
+     * <p>Positions camera at the center of the map.</p>
+     */
+    private void centerCamOnMapCenter() {
+        float mapCenterX = MAP_UNIT * mapWidth/2;
+        float mapCenterY = MAP_UNIT * mapHeight/2;
+        cam.position.set(mapCenterX,mapCenterY,0);
+        cam.update();
+        rend.setView(cam);
+    }
+
+    /**
+     * <p>Centers cam on given player.</p>
+     * @param p Player
+     */
+    private void centerCamOnPlayer(Player p) {
+        float playerX = p.getRobot().getPosition().getX() * MAP_UNIT;
+        float playerY = p.getRobot().getPosition().getY() * MAP_UNIT;
+
+        cam.position.set(playerX + CAM_OFFSET_X, playerY + CAM_OFFSET_Y,0);
+        cam.update();
+        updateRendWithCam();
+    }
+
+    /*
+     * * * * * Interface-implementations
+     */
+
+    @Override
+    public void create() {
+    }
+
+    @Override
+    public void render() {
+    }
+
+    /*
+     * * * * * Input-listening
+     */
+
     @Override
     public boolean scrolled(int amount) {
         zoomCam(((float) amount)/2f);
         return false;
-    }
-
-    @Override
-    public void create() {
     }
 
     @Override
@@ -461,10 +512,6 @@ public class GameScreen extends InputAdapter implements ApplicationListener, Scr
         uiStage.act();
         uiStage.draw();
         cam.update();
-    }
-
-    @Override
-    public void render() {
     }
 
     @Override
@@ -480,9 +527,7 @@ public class GameScreen extends InputAdapter implements ApplicationListener, Scr
     }
 
     @Override
-    public void dispose() {
-        uiStage.dispose();
-        rend.dispose();
+    public void show() {
     }
 
     @Override
@@ -522,9 +567,24 @@ public class GameScreen extends InputAdapter implements ApplicationListener, Scr
         return false;
     }
 
+    /**
+     * <p>Zooms camera.</p>
+     * @param zoom Zoom
+     */
     private void zoomCam(float zoom) {
         cam.zoom += zoom;
         cam.update();
         rend.setView(cam);
     }
+
+    /*
+     * * * * * Other interface-implementations
+     */
+
+    @Override
+    public void dispose() {
+        uiStage.dispose();
+        rend.dispose();
+    }
+
 }
